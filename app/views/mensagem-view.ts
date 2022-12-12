f846b66c96fd7c99f5a77c5ca71e2bd8453af500
John Mota <88064321+John-Mota@users.noreply.github.com>
2022-12-12 11:43:37 -0300

    erro HTMLElement

---
+++ app/views/mensagem-view.ts
@@ -0,0 +1,8 @@
+export class MensagemView {
+
+   private element: HTMLElement;
+
+   constructor(seletor: string) {
+    this.element = document.querySelector(seletor);
+   }
+}
--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,3 +1,6 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+
 export class NegociacoesView {
 
     private elemento: HTMLElement;
@@ -5,7 +8,7 @@ export class NegociacoesView {
     constructor(seletor: string) {
         this.elemento = document.querySelector(seletor);
     }
-    template(): string {
+    template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -16,12 +19,24 @@ export class NegociacoesView {
                 </tr>
             </thead>
             <tbody>
+                ${model.lista().map(negociacao => {
+                    return `
+                    <tr>
+                        <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+
+                        <td>${negociacao.quantidade}</td>
+
+                        <td>${negociacao.valor}</td>
+                    </tr>
+                    `;
+                }).join('')}
             </tbody>
         </table>
         `
     }
 
-    update(): void {
-        this.elemento.innerHTML = this.template();
+    update(model: Negociacoes): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
     }
 }
