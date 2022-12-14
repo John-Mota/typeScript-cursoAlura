8c083f020af1b3f5a925c2e2c6b071aaddacbba0
John Mota <johnmarcos.mota@gmail.com>
2022-12-14 14:53:52 -0300

    module 02 end.

--- app/app.ts
+++ app/app.ts
@@ -7,9 +7,14 @@ import { NegociacoesView } from './views/negociacoes-view.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se 0 for existe.');
+}
+
 
 
