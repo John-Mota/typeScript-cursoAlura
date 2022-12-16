a1686b6a81889499376a2a3f12ffefe8faecaea7
John Mota <johnmarcos.mota@gmail.com>
2022-12-15 23:32:24 -0300

    15.12

--- app/dist/js/decorators/tempo-de-execucao.js
+++ app/dist/js/decorators/tempo-de-execucao.js
@@ -1,11 +1,17 @@
-export function tempoDeExecucao() {
+export function tempoDeExecucao(emSegundos = false) {
     return function (target, propertyKey, descriptor) {
+        let divisor = 1;
+        let unidade = 'milisegundos';
+        if (emSegundos) {
+            divisor = 1000;
+            unidade = 'segundos';
+        }
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000}`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`);
             retorno;
         };
         return descriptor;
--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,6 @@
+import { domInjector } from '../decorators/dom-injector.js';
+import { escapar } from '../decorators/escapar.js';
+import { inspect } from '../decorators/inspect.js';
 import { tempoDeExecucao } from '../decorators/tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
@@ -6,19 +9,21 @@ import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
 export class NegociacaoController {
+    @domInjector('#data')
     private inputData: HTMLInputElement;
+    @domInjector('#quantidade')
     private inputQuantidade: HTMLInputElement;
+    @domInjector('#valor')
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-        this.inputData = <HTMLInputElement>document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
-        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
+
+    @inspect
     @tempoDeExecucao()
     public adiciona(): void {
         
